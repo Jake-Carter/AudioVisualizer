8adcc82f3ddc4392562576b811cf9a5d7099e4a1
Jake Carter <jake.carter@colorado.edu>
2018-05-09 17:05:09 -0600

    Main driver file

--- AudioVisualizerV3/CodeComposer/main.c
+++ AudioVisualizerV3/CodeComposer/main.c
@@ -1,44 +1,3 @@
-/* --COPYRIGHT--,BSD
- * Copyright (c) 2015, Texas Instruments Incorporated
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- *
- * *  Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- *
- * *  Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- *
- * *  Neither the name of Texas Instruments Incorporated nor the names of
- *    its contributors may be used to endorse or promote products derived
- *    from this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
- * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
- * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
- * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
- * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
- * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
- * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
- * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
- * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
- * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- * --/COPYRIGHT--*/
-//****************************************************************************
-//
-// main.c - MSP-EXP432P401R + Educational Boosterpack MkII - Microphone FFT
-//
-//          CMSIS DSP Software Library is used to perform 512-point FFT on
-//          the audio samples collected with MSP432's ADC14 from the Education
-//          Boosterpack's onboard microhpone. The resulting frequency bin data
-//          is displayed on the BoosterPack's 128x128 LCD.
-//
-//****************************************************************************
 //Jake Carter
 //Audio Visualizer
 //Modified the above example project to receive input from an auxiliary audio input
@@ -136,18 +95,6 @@ void main(void)
 
     // Set SysTick period = 0x1000000000000 = 2^24
     SysTick->LOAD = 0xFFFFFF;
-
-    // Enable global interrupt
-//    __enable_irq();
-
-    // Enable SysTick interrupt
-//    SysTick->CTRL |= SysTick_CTRL_TICKINT_Msk;
-
-//    /* Enabling interrupts */
-//    MAP_UART_enableInterrupt(EUSCI_A0_BASE, EUSCI_A_UART_RECEIVE_INTERRUPT);
-//    MAP_Interrupt_enableInterrupt(INT_EUSCIA0);
-//    MAP_Interrupt_enableSleepOnIsrExit();
-//    MAP_Interrupt_enableMaster();
 
     // Initialize Hann Window
     int n;
